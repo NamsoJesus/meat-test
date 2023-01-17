6df13aa73f7c38e54a284c543842ef26a2d28b15
Developer Ballast Lane <ballastlane@Developers-MacBook-Pro-3.local>
2023-01-17 14:21:25 -0500

    filter changes

--- components/GalleryMain.js
+++ components/GalleryMain.js
@@ -7,22 +7,44 @@ import styles from "../styles/GalleryMain.module.css";
 import { CardProduct } from "./CardProduct";
 
 export const GalleryMain = () => {
- 
+  const [filterSelected, setFilterSelected] = useState("Todos");
   const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [filters, setFilters] = useState(["Todos"]);
+
+  const handleFilter = (e) => {
+    setFilterSelected(e.target.value);
+    updateFilteredProducts(products, e.target.value);
+  };
 
   const getProducts = async () => {
-    try {
-      const resp = await axios.get( `https://5eed24da4cbc340016330f0d.mockapi.io/api/articles`)
-      setProducts(resp.data);
+    try{
+      const resp = await axios.get(
+        `https://5eed24da4cbc340016330f0d.mockapi.io/api/articles?limit=${filterSelected}`
+      )
+        setProducts(resp.data);
+        const category = resp.data.map((product) => product.category);
+        setFilters(["Todos", ...new Set(category)]);
+        updateFilteredProducts(resp.data, filterSelected);
     } catch (err) {
       setProducts([]);
       console.log(err);
     }
   };
 
+  const updateFilteredProducts = (_products, _filter) => {
+    if (filterSelected === "Todos") {
+      setFilteredProducts(_products);
+    } else {
+      setFilteredProducts(
+        _products.filter((product) => product.category === _filter)
+      );
+    }
+  };
+
   useEffect(() => {
     getProducts();
-  }, []);
+  }, [filterSelected]);
 
   return (
     <section className={styles.articles}>
@@ -33,72 +55,41 @@ export const GalleryMain = () => {
         </div>
         <div className={styles.content}>
           <aside className={styles.aside}>
-            <ul className={styles.list}>
-              <li
-                className={`${styles.listItem}`}
-              >
-                <button
-                  type="button"
-                  className={styles.button}
+          <ul className={styles.list}>
+              {filters.map((filter) => (
+                <li
+                  key={filter}
+                  className={`${styles.listItem} ${
+                    filterSelected === filter ? styles.listItemActive : null
+                  }`}
                 >
-                  TODOS
-                  <Image
-                    src={arrow}
-                    alt="Flecha"
-                    className={`${styles.arrow}`}
-                  />
-                </button>
-              </li>
-              <li
-                className={`${styles.listItem}`}
-              >
-                <button
-                  type="button"
-                  className={styles.button}
-                >
-                  RECETAS
-                  <Image
-                    src={arrow}
-                    alt="Flecha"
-                    className={`${styles.arrow}`}
-                  />
-                </button>
-              </li>
-              <li
-                className={`${styles.listItem}`}
-              >
-                <button
-                  type="button"
-                  className={styles.button}
-                >
-                  PRODUCTOS
-                  <Image
-                    src={arrow}
-                    alt="Flecha"
-                    className={`${styles.arrow}`}
-                  />
-                </button>
-              </li>
-              <li
-                className={`${styles.listItem}`}
-              >
-                <button
-                  type="button"
-                  className={styles.button}
-                >
-                  CONSEJOS
-                  <Image
-                    src={arrow}
-                    alt="Flecha"
-                    className={`${styles.arrow}`}
-                  />
-                </button>
-              </li>
+                  <button
+                    onClick={handleFilter}
+                    type="button"
+                    value={filter}
+                    className={styles.button}
+                  >
+                    {filter}
+                    <Image
+                      src={arrow}
+                      alt="Flecha"
+                      className={`
+                          ${styles.arrow}
+                          ${
+                            filterSelected === filter
+                              ? styles.arrowActive
+                              : null
+                          }
+                        `}
+                    />
+                  </button>
+                </li>
+              ))}
             </ul>
           </aside>
           <div className={styles.articlesWrapper}>
             <ol className={styles.articlesList}>
-              {products.map((product) => (
+            {filteredProducts.map((product) => (
                 <CardProduct key={product.id} product={product} />
               ))}
             </ol>
